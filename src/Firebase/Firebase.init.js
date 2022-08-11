554d8df165622f9db57d990f4dbcf1a0170a6085
mdbappa0001 <abutanif366@gmail.com>
2022-08-11 19:53:02 +0600

    components added

---
+++ src/Firebase/Firebase.init.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDgHd51MG49v2AEcqLs-ycBDRWlLmrFtm8",
+  authDomain: "travel-agency-7843a.firebaseapp.com",
+  projectId: "travel-agency-7843a",
+  storageBucket: "travel-agency-7843a.appspot.com",
+  messagingSenderId: "1088395635835",
+  appId: "1:1088395635835:web:e8c0efa3e0783d265c06de"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
